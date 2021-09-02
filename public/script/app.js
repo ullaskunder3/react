8cbba087c252bc4a16cb50af52b92faeb19c57f3
ullas <ullaskunder3@gmail.com>
2021-09-03 01:47:19 +0530

    component class with render which return()

--- public/script/app.js
+++ public/script/app.js
@@ -1,15 +1,41 @@
 'use strict';
 
-var people = [{ name: 'ullas' }, { name: 'kunder' }, { name: 'Villas' }];
-var template = React.createElement(
-    'ul',
-    null,
-    people.map(function (person) {
-        return React.createElement(
-            'li',
-            { key: person.name },
-            person.name
-        );
-    })
-);
-ReactDOM.render(template, document.getElementById('app'));
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var NameList = function (_React$Component) {
+    _inherits(NameList, _React$Component);
+
+    function NameList() {
+        _classCallCheck(this, NameList);
+
+        return _possibleConstructorReturn(this, (NameList.__proto__ || Object.getPrototypeOf(NameList)).apply(this, arguments));
+    }
+
+    _createClass(NameList, [{
+        key: 'render',
+        value: function render() {
+            var people = [{ name: 'ullas' }, { name: 'kunder' }, { name: 'Villas' }];
+            return React.createElement(
+                'ul',
+                null,
+                people.map(function (person) {
+                    return React.createElement(
+                        'li',
+                        { key: person.name },
+                        person.name
+                    );
+                })
+            );
+        }
+    }]);
+
+    return NameList;
+}(React.Component);
+
+ReactDOM.render(React.createElement(NameList, null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-const people = [
-    { name: 'ullas' },
-    { name: 'kunder' },
-    { name: 'Villas' }
-]
-const template = (
-    <ul>
-        { people.map(person => (
-            <li key={ person.name }>{ person.name }</li>
-        )) }
-    </ul>
-)
-ReactDOM.render(template, document.getElementById('app'));
+class NameList extends React.Component {
+    render() {
+        const people = [
+            { name: 'ullas' },
+            { name: 'kunder' },
+            { name: 'Villas' }
+        ]
+        return (
+            <ul>
+                { people.map(person => (
+                    <li key={ person.name }>{ person.name }</li>
+                )) }
+            </ul>
+        )
+    }
+}
+
+ReactDOM.render(<NameList />, document.getElementById('app'));
