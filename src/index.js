ee3ed093c9a03d73149a3d9bcf7a529fd00b626f
ullas <ullaskunder3@gmail.com>
2021-09-08 01:58:20 +0530

    index.js main or parent component ReactDOM rendering App component

--- src/index.js
+++ src/index.js
@@ -1,57 +1,6 @@
-class DeveloperList extends React.Component {
-    render() {
-        console.log(this.props.developerlist);
-        return (
-            <ol className="developer-list">
-                {
-                    this.props.developerlist.map((developer) => (
-                        <li key={ developer.id } className="developer-item">
-                            <div className= "developer-avatar" style={ {
-                                backgroundImage: `url(${developer.avatarUrl})`
-                            } } />
-                            <div className="developer-details">
-                                <p>{ developer.name }</p>
-                                <p>{ developer.email }</p>
-                            </div>
-                            <button className="contact-remove">
-                            </button>
-                        </li>
-                    ))
-                }
-            </ol>
-        )
-    }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        // Lets use reference
-        const developerlist = [
-            {
-                id: 'DevJr1',
-                name: 'Ullas Kunder',
-                email: "ullaskunder3@gmail.com",
-                "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
-            },
-            {
-                id: 'DevJr2',
-                name: 'Captain',
-                email: "ullaskunder3@gmail.com",
-                "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
-            },
-            {
-                id: 'DevJr3',
-                name: 'America',
-                email: "ullaskunder3@gmail.com",
-                "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
-            }
-        ]
-        return (
-            <div>
-                <DeveloperList developerlist={ developerlist } />
-            </div>
-        )
-    }
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
