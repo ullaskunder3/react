1119ca242bc35b9b92cc34f7c9e1580890a0b3d3
ullas <ullaskunder3@gmail.com>
2021-09-08 01:52:49 +0530

    list component with data prop

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import DeveloperList from './DeveloperList.js';
+
+const developerlist = [
+    {
+        id: 'DevJr1',
+        name: 'Ullas Kunder',
+        email: "ullaskunder3@gmail.com",
+        "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+    },
+    {
+        id: 'DevJr2',
+        name: 'Captain',
+        email: "example@gmail.com",
+        "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+    },
+    {
+        id: 'DevJr3',
+        name: 'America',
+        email: "userDevmail@gmail.com",
+        "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+    }
+]
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <DeveloperList developerlist={developerlist}/>
+            </div>
+        )
+    }
+}
+export default App
