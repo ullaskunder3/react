53d3a541cbff4bd992b6bece4f56d921c065bf53
ullas <ullaskunder3@gmail.com>
2021-09-09 01:16:27 +0530

    stateless functional component

--- src/DeveloperList.js
+++ src/DeveloperList.js
@@ -1,29 +1,72 @@
-import React from 'react';
+import React from "react";
 
-class DeveloperList extends React.Component{
-    render(){
-        console.log(this.props);
-        return(
-            <ol className="developer-list">
-            {
-                this.props.developerlist.map((developer) => (
-                    <li key={ developer.id } className="developer-item">
-                        <div className= "developer-avatar" style={ {
-                            backgroundImage: `url(${developer.avatarUrl})`
-                        } } />
-                        <div className="developer-details">
-                            <span>{developer.id}</span>
-                            <p>{ developer.name }</p>
-                            <p>{ developer.email }</p>
-                        </div>
-                        <button className="contact-remove">
-                        </button>
-                    </li>
-                ))
-            }
+// stateless functional component
+function DeveloperList(props) {
+    return (
+        <ol className="developer-list">
+
+            { props.developerlist.map((developer) => (
+
+                <li key={ developer.id } className="developer-item">
+                    <div
+                        className="developer-avatar"
+                        style={ { backgroundImage: `url(${developer.avatarUrl})` } }
+                    />
+
+                    <div className="developer-details">
+                        <span>{ developer.id }</span>
+                        <p>{ developer.name }</p>
+                        <p>{ developer.email }</p>
+                    </div>
+
+                    <button className="contact-remove"></button>
+                </li>
+
+            )) }
         </ol>
-        )
-    }
+    );
 }
+
+// class DeveloperList extends React.Component{
+//     render(){
+//         // this.propes is an object contains arry property
+//         console.log(this.props);
+//         return(
+//             //
+//             <ol className="developer-list">
+//                 {/* object like syntax inside that we will have expression */}
+//             {
+//                 this.props.developerlist.map((developer) => (
+//                     <li key={ developer.id }
+//                         className="developer-item">
+
+//                         <div className= "developer-avatar"
+//                             style={ { backgroundImage: `url(${developer.avatarUrl})`} }
+//                              />
+
+//                         <div className="developer-details">
+//                             <span>{developer.id}</span>
+//                             <p>{ developer.name }</p>
+//                             <p>{ developer.email }</p>
+//                         </div>
+
+//                         <button className="contact-remove"></button>
+//                     </li>
+//                 ))
+//             }
+//         </ol>
+//         )
+//     }
+// }
+
 //exporting Component
-export default DeveloperList
+export default DeveloperList;
+
+// each child in an array or iterator should have unique "key" props.
+/**
+ * The reason we need to add key = {} to each child is
+ * eventually one of those list item may change
+ * and by having unique key attribute on each list item
+ * react will know which item in the list has changed
+ * and recreate only that item rather than whole list item
+ */
