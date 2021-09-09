7c8f94ea8ff9b2a196ff7b4aeb8c34f639a2496b
ullas <ullaskunder3@gmail.com>
2021-09-09 14:10:19 +0530

    set state to a component

--- src/App.js
+++ src/App.js
@@ -2,32 +2,36 @@ import React from 'react';
 
 import DeveloperList from './DeveloperList.js';
 
-const developerlist = [
-    {
-        id: 'DevJr1',
-        name: 'Ullas Kunder',
-        email: "ullaskunder3@gmail.com",
-        "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
-    },
-    {
-        id: 'DevJr2',
-        name: 'Captain',
-        email: "example@gmail.com",
-        "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
-    },
-    {
-        id: 'DevJr3',
-        name: 'America',
-        email: "userDevmail@gmail.com",
-        "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+class App extends React.Component {
+
+    state = {
+        developerlist: [
+            {
+                id: 'DevJr1',
+                name: 'Ullas Kunder',
+                email: "ullaskunder3@gmail.com",
+                "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+            },
+            {
+                id: 'DevJr2',
+                name: 'Captain',
+                email: "example@gmail.com",
+                "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+            },
+            {
+                id: 'DevJr3',
+                name: 'America',
+                email: "userDevmail@gmail.com",
+                "avatarUrl": "https://randomuser.me/api/portraits/med/men/1.jpg"
+            }
+        ]
     }
-]
-class App extends React.Component{
-    render(){
-        return(
+
+    render() {
+        return (
             <div>
-                {/* passing array of object to component */}
-                <DeveloperList developerlist={developerlist}/>
+                {/* passing state */}
+                <DeveloperList developerlist={ this.state.developerlist } />
             </div>
         )
     }
